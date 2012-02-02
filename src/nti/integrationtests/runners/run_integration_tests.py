56ec915cb42f8f09e7114c8140de64b0b7bce505
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-02-02 18:17:44 +0000

    allow passing the port and root_dir
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@4788 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/runners/run_integration_tests.py
+++ src/nti/integrationtests/runners/run_integration_tests.py
@@ -2,8 +2,10 @@
 
 import os
 import sys
+import tempfile 
 import argparse
 
+from nti.integrationtests.dataserver.server import PORT
 from nti.integrationtests.runners import test_runner
 
 def main(args = None):
@@ -13,13 +15,18 @@ def main(args = None):
 	parser = argparse.ArgumentParser(prog='Integration Tests')
 	parser.add_argument('-uc', '--use_coverage', help='use coverage', action='store_true', default = False)
 	parser.add_argument('-cr', '--coverage_report', help='create coverage report', action='store_true', default = False)
+	parser.add_argument('-rd', '--root_dir', help='root directory', required=False)
+	parser.add_argument('-p', '--port', help='server port', type=int, required=False, default=PORT)
 	opts = parser.parse_args(args)
 
 	use_coverage = True if opts.use_coverage else False
 	coverage_report = True if opts.coverage_report else False
+	root_dir = opts.root_dir if opts.root_dir else tempfile.mkdtemp(prefix="ds.data.int.", dir="/tmp")
+	port = opts.port if opts.port else PORT
 	
 	dirname = os.path.join(os.path.dirname( __file__ ), '..', 'integration')
-	test_runner( path = dirname, use_coverage=use_coverage, coverage_report=coverage_report)
+	test_runner(path=dirname, use_coverage=use_coverage, coverage_report=coverage_report,\
+				port=port, root_di=root_dir)
 
 if __name__ == '__main__':
 	main()
