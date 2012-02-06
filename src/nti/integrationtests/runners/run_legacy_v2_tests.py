cd75a254cdad663a6a6a2d6613882a6873b6711a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-02-06 15:06:12 +0000

    run on different ports
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@4862 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/runners/run_legacy_v2_tests.py
+++ src/nti/integrationtests/runners/run_legacy_v2_tests.py
@@ -4,11 +4,12 @@ import tempfile
 
 from nti.integrationtests.runners import run_test_suite
 from nti.integrationtests.legacy.ServerTest_v2 import test_suite
+from nti.integrationtests.dataserver.server import get_open_port
 from nti.integrationtests.dataserver.server import DataserverProcess
 
 def main(port=None, root_dir=None):	
 	root_dir = root_dir or tempfile.mkdtemp(prefix="ds.data.", dir="/tmp")
-	dsprocess = DataserverProcess(port=port, root_dir=root_dir)
+	dsprocess = DataserverProcess(port=get_open_port(), root_dir=root_dir)
 	run_test_suite (test_suite(), dsprocess)
 			
 if __name__ == '__main__':
