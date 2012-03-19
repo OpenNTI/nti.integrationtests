e90bcd8f41a06f4bcea1bb44224906eb0130935a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-03-19 17:55:47 +0000

    Logan's changes to test images.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@5919 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/image/__init__.py
+++ src/nti/integrationtests/image/__init__.py
@@ -3,49 +3,114 @@ import math, operator
 from PIL import Image
 import unittest
 
-class Comparisons(unittest.TestCase):
+class FileStatis(object):
     
-    basePicDir = os.path.dirname(__file__) + "/basepics/"
-    testPicDir = "/Volumes/WebServer/Documents/prealgebra/"
+    file_name = None
     
-    @classmethod
-    def setUpClass(cls):
+    base_pic_path = None
+    test_pic_path = None
+    rel_path = None
     
-        assert os.path.exists(cls.basePicDir), "cannot find base directory pictures"
-        assert os.path.exists(cls.testPicDir), "cannot find test directory pictures"
+    file_in_base = False
+    file_in_test = False
+    file_statis = None
+    
+    file_size = 0
+    
+    def __init__(self, file_name, file_rel_path, file_size, base_dir_path=None, test_dir_path=None, file_in_base=False, file_in_test=False):
+        self.file_name = file_name
+        self.rel_path = file_rel_path
+        self.file_size = file_size
+        if base_dir_path: self.base_pic_path = base_dir_path + file_rel_path
+        if test_dir_path: self.test_pic_path = test_dir_path + file_rel_path
+        if file_in_base: self.file_in_base = True 
+        if file_in_test: self.file_in_test = True
+    
+    def get_file_name(self):
+        return self.file_name
+    
+    def found_base_file(self, path):
+        self.base_pic_path = path
+        self.file_in_base = True
+    
+    def found_test_file(self, path):
+        self.test_pic_path = path
+        self.file_in_test = True
+    
+    def check_statis(self):
+        if not self.file_in_base: self.file_statis = 'pic not in base pics'
+        elif not self.file_in_test: self.file_statis = 'pic not in test pics'
+        else:
+            print self.base_pic_path
+            print self.test_pic_path
+            bh = Image.open(self.base_pic_path).histogram()
+            th = Image.open(self.test_pic_path).histogram()
+            rms = math.sqrt(reduce(operator.add, map(lambda a,b: (a-b)**2, bh, th))/len(bh))
+            if rms is 0: self.file_statis = 'images are equal'
+            else: self.file_statis = 'images are not equal'
+    
+extentions = ['jpg', 'png', 'bmp', 'gif']
 
-    def compare(self, relPicPath):
+def check_paths(basePicDir, testPicDir):
+    assert os.path.exists(basePicDir), "cannot find base directory pictures"
+    assert os.path.exists(testPicDir), "cannot find test directory pictures"
+                
+def store_pics(pic_data, abs_path, rel_path="/", is_base_pic=True):
+    path = abs_path + rel_path
+    pic_data['Items'] = pic_data.get('Items', [])
+    items = os.listdir(path)
+    for item in items:
+        if os.path.isdir(path + '/' + item): 
+            pic_data[item] = pic_data.get(item, {})
+            store_pics(pic_data[item], abs_path, rel_path + '/' + item, is_base_pic)
+        else:
+            not_found = True
+            for pic in pic_data['Items']:
+                if item == pic.get_file_name() and is_base_pic:
+                    pic.found_base_file(path)
+                    not_found = False
+                elif item == pic.get_file_name():
+                    pic.found_test_file(path)
+                    not_found = False
+            if not_found and len(item.split(".")) is 2 and item.split(".")[1] in extentions and is_base_pic:
+                newPic = FileStatis(item, rel_path, os.path.getsize(path), base_dir_path=abs_path, file_in_base=True)
+                pic_data['Items'].append(newPic)
+            elif not_found and len(item.split(".")) is 2 and item.split(".")[1] in extentions:
+                newPic = FileStatis(item, rel_path, os.path.getsize(path), base_dir_path=abs_path, file_in_test=True)
+                pic_data['Items'].append(newPic)
         
-        basePicPath = self.basePicDir + relPicPath
-        testPicPath = self.testPicDir + relPicPath
+def clean_up():pass
         
-        if not os.path.exists(basePicPath) and not os.path.exists(testPicPath):
-            assert False, "could not find either picture"
-        
-        elif not os.path.exists(basePicPath):
-            assert False, 'could not find the base picture'
-        
-        elif not os.path.exists(testPicPath):
-            assert False, 'could not find the test picture'
-        
-        bh = Image.open(basePicPath).histogram()
-        th = Image.open(testPicPath).histogram()
-        
-        rms = math.sqrt(reduce(operator.add, map(lambda a,b: (a-b)**2, bh, th))/len(bh))
-        
-        assert rms == 0
-
-    def test_icons_chapter_C1(self):
-        relPicPath = "icons/chapters/C1.png"
-        self.compare(relPicPath)
+def write_pic_to_file(pic_data, txt_file_path):
+    for key in pic_data.keys():
+        if isinstance(pic_data[key], dict):
+            write_pic_to_file(pic_data[key], txt_file_path)
+        elif isinstance(pic_data[key], list):
+            for pic in pic_data[key]:
+                pic.check_statis()
+                print 'name:', pic.file_name, ', path: ', pic.rel_path, ', file size: ', pic.file_size, ', result: ', pic.file_statis
     
-    def test_icons_up(self): 
-        relPicPath = "icons/up.gif"
-        self.compare(relPicPath)
-
-    def test_thumbnails_sec_negation(self): 
-        relPicPath = "thumbnails/sec-negation.png"
-        self.compare(relPicPath)
+        
+def main(args=None):
+    base_pic_dir = os.path.dirname(__file__) + "/setbase"
+    test_pic_dir = os.path.dirname(__file__) + "/settest"
+    txt_file     = os.path.dirname(__file__) + "/output"
+    if args:
+        base_pic_dir = args[0]
+        test_pic_dir = args[1]
+    check_paths(base_pic_dir, test_pic_dir)
+    pic_data = {}
+    store_pics(pic_data, base_pic_dir)
+    store_pics(pic_data, test_pic_dir, is_base_pic=False)
+    write_pic_to_file(pic_data, txt_file)
+    
+    
+#    for item in pic_data['Items']:
+#        print item.get_file_name()
+#    print '\n\n\n'
+#    for item in pic_data['Items']:
+#        print item.get_file_name()
+    
     
 if __name__ == '__main__':
-    unittest.main()
+    main()
