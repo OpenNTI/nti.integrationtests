55e398efb200cb8e557cbe70cef5e9f2d87e095e
Logan Testi <logan.testi@nextthought.com>
2012-03-22 22:44:48 +0000

    how the path is retrieved from a dictionary storing a path to the keys of the dictionary being the relative files of the path and the values being the files
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@6044 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/image/__init__.py
+++ src/nti/integrationtests/image/__init__.py
@@ -7,22 +7,23 @@ extentions = ['jpg', 'png', 'bmp', 'gif']
 def check_paths(basePicDir, testPicDir, txt_file):
     assert os.path.exists(basePicDir), "cannot find base directory pictures"
     assert os.path.exists(testPicDir), "cannot find test directory pictures"
-    assert os.path.exists(txt_file), "cannot find target txt_file"
+    assert os.path.exists(txt_file),   "cannot find target txt_file"
                 
-def store_pics(abs_path, rel_path="/"):
-    path = abs_path + rel_path
+def store_pics(abs_path):
+    abs_path = abs_path + '/' if abs_path[len(abs_path) -1] != '/' else abs_path
     pic_data = {}
-    items = os.listdir(path)
+    items = os.listdir(abs_path)
     for item in items:
-        if os.path.isdir(path + '/' + item): 
-            pic_data[item] = store_pics(abs_path, rel_path + item + '/')
+        if os.path.isdir(abs_path + '/' + item): 
+            pic_data[item] = store_pics(abs_path + item)
         else:
             if len(item.split(".")) is 2 and item.split(".")[1] in extentions:
-                pic_data[item] = rel_path + item
+                pic_data[item] = item
     return pic_data
-#                newPic = FileStatis(item, rel_path, os.path.getsize(path+item), base_dir_path=abs_path, file_in_base=True)
         
-def compare_pics(base_pic_data, test_pic_data, f, files):
+def compare_pics(base_pic_data, test_pic_data, f, files, rel_path=''):
+    files['base_pic_dir'] = files['base_pic_dir'] + '/' if files['base_pic_dir'][len(files['base_pic_dir']) -1] != '/' else files['base_pic_dir']
+    files['test_pic_dir'] = files['test_pic_dir'] + '/' if files['test_pic_dir'][len(files['test_pic_dir']) -1] != '/' else files['test_pic_dir']
     base_keys = base_pic_data.keys()
     test_keys = test_pic_data.keys()
     for key in base_keys:
@@ -32,21 +33,29 @@ def compare_pics(base_pic_data, test_pic_data, f, files):
             del(base_pic_data[key])
             del(test_pic_data[key])
             if isinstance(base_key, dict) and isinstance(test_key, dict):
-                compare_pics(base_key, test_key, f, files)
+                local_path = rel_path + key + '/'
+                compare_pics(base_key, test_key, f, files, local_path)
             elif isinstance(base_key, dict): pass #deal with later
             elif isinstance(test_key, dict): pass #deal with later
             else:
-                bh = Image.open(files['base_pic_dir'] + base_key).histogram()
-                th = Image.open(files['test_pic_dir'] + test_key).histogram()
+                bh = Image.open(files['base_pic_dir'] + rel_path + base_key).histogram()
+                th = Image.open(files['test_pic_dir'] + rel_path + test_key).histogram()
                 rms = math.sqrt(reduce(operator.add, map(lambda a,b: (a-b)**2, bh, th))/len(bh))
+                print_path = '~/' + rel_path + base_key
+                base_file_size = os.path.getsize(files['base_pic_dir'] + base_key)
+                test_file_size = os.path.getsize(files['test_pic_dir'] + test_key)
                 if rms == 0.0:
-                    f.write('name: ' + key + ', file path: ' + base_key + ', file size: ' + str(os.path.getsize(files['base_pic_dir'] + base_key))  + ', assessment: pictures are equivalent' + '\n')
+                    f.write(('name: %s, file path: %s, file size: %d, assessment: %s\n') % (key, print_path, base_file_size, 'pictures are equivalent'))
                 else:
-                    f.write('name: ' + key + ', file path: ' + base_key + ', file size of base: ' + str(os.path.getsize(files['base_pic_dir'] + base_key))  + ', file size of test: ' + str(os.path.getsize(files['test_pic_dir'] + test_key))  + ', assessment: pictures are not equivalent' + '\n')
+                    f.write(('\n\n\nname: %s, file path: %s, file size(base): %d, file size(test): %d, assessment: %s\n') % (key, print_path, base_file_size, test_file_size, 'pictures are not equivalent'))
     for key in base_pic_data.keys():
-        f.write('name: ' + key + ', file path: ' + base_pic_data[key] + ', file size: ' + str(os.path.getsize(files['base_pic_dir'] + base_pic_data[key]))  + ', assessment: picture only found in base pics' + '\n')
+        print_path = '~/' + rel_path + key
+        file_size = os.path.getsize(files['base_pic_dir'] + key)
+        f.write(('name: %s, file path: %s, file size: %d, assessment: %s\n') % (key, print_path, file_size, 'picture only found in base pics'))
     for key in test_pic_data.keys():
-        f.write('name: ' + key + ', file path: ' + test_pic_data[key] + ', file size: ' + str(os.path.getsize(files['test_pic_dir'] + test_pic_data[key]))  + ', assessment: picture only found in test pics' + '\n')
+        print_path = '~/' + rel_path + key
+        file_size = os.path.getsize(files['test_pic_dir'] + key)
+        f.write(('name: %s, file path: %s, file size: %d, assessment: %s\n') % (key, print_path, file_size, 'picture only found in test pics'))
                 
 def write_pic_to_file(base_pic_data, test_pic_data, files):
     f = open(files['txt_file'], 'w')
