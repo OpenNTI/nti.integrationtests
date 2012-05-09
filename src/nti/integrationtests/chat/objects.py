0accbf1bc72e4394b890b534d23f72c83e23c31c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-05-09 22:03:15 +0000

    write a method to simulate a chat
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@7217 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/chat/objects.py
+++ src/nti/integrationtests/chat/objects.py
@@ -265,21 +265,22 @@ def run_chat(containerId, host_user, invitees, entries=None, delay=0.25,
 
 	return result
 
-if __name__ == '__main__':
-	cid = 'tag:nextthought.com,2011-10:AOPS-HTML-prealgebra.0'
+def simulate(users, containerId, entries=None, delay=2, server='localhost', port=8081,
+			 max_heart_beats=3, use_threads=True):
+	
+	users = max(min(abs(users), 50), 2)
+	entries = abs(entries) if entries else 50
+	
 	host = 'test.user.1@nextthought.com'
-	users =['test.user.%s@nextthought.com' % s for s in range(2, 11)]
+	users =['test.user.%s@nextthought.com' % s for s in range(2, users+1)]
 	all_users = [host] + users
-	
-	entries = 50
-	server = 'localhost' #'192.168.0.151'
-	
+			
 	def create_fl(username):
 		import uuid
 		from nti.integrationtests.dataserver.client import DataserverClient
 		from nti.integrationtests.dataserver.server import DEFAULT_USER_PASSWORD
 		
-		endpoint = 'http://%s:%s/dataserver2' % (server, 8081)
+		endpoint = 'http://%s:%s/dataserver2' % (server, port)
 		ds = DataserverClient(endpoint=endpoint, credentials=(username, DEFAULT_USER_PASSWORD))
 		list_name = 'cfl-%s-%s' % (username, str(uuid.uuid4()).split('-')[0])
 		
@@ -290,8 +291,13 @@ if __name__ == '__main__':
 	for username in all_users:
 		create_fl(username)
 		
-	result = run_chat(cid, host, users, entries=entries, delay=2, use_threads=True, server=server, max_heart_beats=3)
+	result = run_chat(containerId, host, users, entries=entries, delay=delay,
+					  use_threads=use_threads, server=server, port=port, max_heart_beats=max_heart_beats)
+	
 	for r in result:
 		print r.username, len(list(r.sent)), len(list(r.received)), r.traceback
+	
+if __name__ == '__main__':
+	simulate(4, 'tag:nextthought.com,2011-10:AOPS-HTML-prealgebra.0', 3)
 
 
