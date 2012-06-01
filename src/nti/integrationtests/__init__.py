c10a08ea335835a192f492198f74556a5509e310
Jason Madden <jason.madden@nextthought.com>
2012-06-01 00:44:09 +0000

    Bump to latest gunicorn; unit and integration tests pass. Fix bug where integration tests could DELETE YOUR ENTIRE TMP DIRECTORY\!\!\! Argh.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@7756 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/__init__.py
+++ src/nti/integrationtests/__init__.py
@@ -14,10 +14,9 @@ from nti.integrationtests.dataserver.server import DEFAULT_USER_PASSWORD
 from nti.integrationtests.dataserver.client import DataserverClient
 from nti.integrationtests.dataserver.client import ROOT_ITEM
 
-# ====================
 
 def generate_ntiid(date=None, provider='nti', nttype=None, specific=None):
-	
+
 	def escape_provider( provider ):
 		return provider.replace( ' ', '_' ).replace( '-', '_' )
 
@@ -27,33 +26,32 @@ def generate_ntiid(date=None, provider='nti', nttype=None, specific=None):
 	date_seconds = date if isinstance( date, numbers.Real ) and date > 0 else time.time()
 	date = datetime.date( *time.gmtime(date_seconds)[0:3] )
 	date_string = date.isoformat()
-	
+
 	provider = escape_provider( str(provider) ) + '-'
 	specific = '-' + specific if specific else '-' + str(time.clock())
-	
+
 	result = 'tag:nextthought.com,%s:%s%s%s' % (date_string, provider, nttype, specific )
 	return result
-	
-# ====================
+
 
 class DataServerTestCase(unittest.TestCase):
 
 	# well-known IDs
-	
+
 	DATE = "2011-10"
 	ROOT = "tag:nextthought.com,2011-10:Root"
-	
+
 	TYPE_OID = 'OID'
-	
+
 	TYPE_ROOM = 'MeetingRoom'
 	TYPE_MEETINGROOM = TYPE_ROOM
-	
+
 	TYPE_HTML = 'HTML'
 	TYPE_QUIZ = 'Quiz'
-	
+
 	TYPE_CLASS = 'Class'
 	TYPE_CLASS_SECTION = 'ClassSection'
-	
+
 	TYPE_MEETINGROOM_GROUP = TYPE_ROOM + ':Group'
 	TYPE_MEETINGROOM_CLASS = TYPE_ROOM + ':Class'
 	TYPE_MEETINGROOM_SECT  = TYPE_ROOM + ':ClassSection'
@@ -61,15 +59,15 @@ class DataServerTestCase(unittest.TestCase):
 	TYPE_TRANSCRIPT_SUMMARY = 'TranscriptSummary'
 
 	# class vars
-	
+
 	port = PORT
 	root_dir = DATASERVER_DIR
-	
+
 	user_names = []
 	root_item = ROOT_ITEM
-	
+
 	default_user_password = DEFAULT_USER_PASSWORD
-	
+
 	# We need to start a dataserver (and stop it)
 	# if there is not already one running
 	@classmethod
@@ -84,40 +82,40 @@ class DataServerTestCase(unittest.TestCase):
 	def setUp(self):
 		endpoint = self.get_endpoint()
 		self.ds = DataserverClient(endpoint)
-		
+
 	@property
 	def client(self):
 		return self.ds
-	
+
 	def get_endpoint(self):
 		if hasattr(self, 'endpoint'):
 			return self.endpoint
 		return self.resolve_endpoint(port = self.port)
-	
+
 	# ======================
-	
+
 	@classmethod
 	def resolve_endpoint(cls, host=None, port=None):
 		return DataserverProcess.resolve_endpoint(host, port)
-	
+
 	@classmethod
 	def new_client(cls, credentials=None, endpoint=None):
 		clt = DataserverClient(endpoint or DataserverProcess.ENDPOINT2)
 		if credentials:
 			clt.set_credentials(credentials)
 		return clt
-	
+
 	# ======================
-	
+
 	@classmethod
 	def start_server(cls):
 		cls.process = DataserverProcess(port=cls.port, root_dir=cls.root_dir)
 		cls.process.start_server()
-		
+
 	@classmethod
 	def static_initialization(cls):
 		pass
-	
+
 	@classmethod
 	def generate_ntiid(cls, date=None, provider='nti', nttype=None, specific=None):
 		return generate_ntiid(date=date, provider=provider, nttype=nttype, specific=specific)
--- src/nti/integrationtests/dataserver/server.py
+++ src/nti/integrationtests/dataserver/server.py
@@ -21,16 +21,19 @@ DEFAULT_USER_PASSWORD = 'temp001'
 
 PORT = int(os.getenv('PORT', '8081'))
 SERVER_HOST = os.getenv('SERVER_HOST', 'localhost')
-DATASERVER_DIR = os.getenv('DATASERVER_DIR', '~/tmp')
+# Must be extremely careful with this next one. Some clients
+# assume they own it and can delete the entire thing, clearly not right,
+# but cater to it by using a throw-away directory if one is not provided.
+DATASERVER_DIR = os.getenv('DATASERVER_DIR') or os.path.join( tempfile.gettempdir(), 'DEFAULT_INT_TEST_DIR' )
 SERVER_CONFIG = os.getenv('SERVER_CONFIG', os.path.join(os.path.dirname(__file__), "../../../../config/development.ini"))
 COVERAGE_CONFIG = os.getenv('COVERAGE_CONFIG', os.path.join(os.path.dirname(__file__), "../../../../config/coverage_run.cfg"))
 
-		
+
 class DataserverProcess(object):
 
 	ENDPOINT = 'http://%s:%s/dataserver' % (SERVER_HOST, PORT)
 	ENDPOINT2 = 'http://%s:%s/dataserver2' % (SERVER_HOST, PORT)
-	
+
 	SERVER_DELAY = 3
 	KEY_TEST_WAIT= 'TEST_WAIT'
 
@@ -41,8 +44,8 @@ class DataserverProcess(object):
 		protocol = 'https' if is_secure else 'http'
 		result = '%s://%s:%s/dataserver2' % (protocol, host, port)
 		return result
-	
-	def __init__(self, port=PORT, root_dir=DATASERVER_DIR):
+
+	def __init__(self, port=PORT, root_dir=None):
 		self.process = None
 		self.port = int(port) if port else PORT
 		self.endpoint = self.resolve_endpoint(SERVER_HOST, self.port)
@@ -52,7 +55,7 @@ class DataserverProcess(object):
 		target.port = self.port
 		target.root_dir = self.root_dir
 		target.endpoint = self.endpoint
-		
+
 	def is_running(self):
 		return self._send_message(SERVER_HOST, self.port)
 	isRunning = is_running
@@ -72,45 +75,45 @@ class DataserverProcess(object):
 			return False
 		finally:
 			sock.close()
-		
+
 	# -----------------------------------
-	
+
 	def start_server(self, block_interval_seconds=1, max_wait_secs=30, *arg, **kwargs):
 		self._start_process(block_interval_seconds, max_wait_secs, use_coverage=False, \
 							root_dir=self.root_dir, port=self.port, **kwargs)
 
 	startServer = start_server
-		
+
 	def start_server_with_coverage(self, block_interval_seconds=1, max_wait_secs=30, *arg, **kwargs):
 		self._start_process(block_interval_seconds, max_wait_secs, use_coverage=True, \
 							root_dir=self.root_dir, port=self.port, *arg, **kwargs)
 
 	startServerWithCoverage = start_server_with_coverage
-	
+
 	def _start_process(self, block_interval_seconds=1, max_wait_secs=30, *arg, **kwargs):
 
 		if self.process or self.is_running():
 			logger.debug( "Dataserver already running.  Won't start a new one" )
 		else:
-		
+
 			port = int(kwargs.get('port', PORT))
 			sync_changes = kwargs.get('sync_changes', None)
 			use_coverage = kwargs.get('use_coverage', False)
 			coverage_rcfile = kwargs.get('rcfile', COVERAGE_CONFIG)
 			root_dir = os.path.expanduser(kwargs.get('root_dir', DATASERVER_DIR))
 			pserve_ini_file = kwargs.get('pserve_ini_file', None) or SERVER_CONFIG
-						
+
 			logger.info( 'Starting dataserver (%s,%s)', port, root_dir)
-			
-			pserve_ini_file = self._rewrite_pserve_config(	config=pserve_ini_file, 
-															root_dir=root_dir, 
+
+			pserve_ini_file = self._rewrite_pserve_config(	config=pserve_ini_file,
+															root_dir=root_dir,
 															port=port,
 															sync_changes=sync_changes)
 			if use_coverage:
 				self._writer_supervisor_config_coverage(root_dir, pserve_ini_file, coverage_rcfile)
 			else:
 				self._write_supervisor_config(root_dir, pserve_ini_file)
-			
+
 			command = os.path.join(os.path.dirname(sys.executable), 'supervisord')
 			args = [command, '-c', os.path.join(root_dir, 'etc', 'supervisord_dev.conf')]
 			devnull = open("/dev/null", 'w') if 'DATASERVER_NO_REDIRECT' not in os.environ else None
@@ -122,96 +125,96 @@ class DataserverProcess(object):
 			while elapsed <= max_wait_secs and not self.isRunning():
 				time.sleep(block_interval_seconds)
 				elapsed = elapsed + block_interval_seconds
-	
+
 			if elapsed >= max_wait_secs:
 				raise Exception("Could not start data server")
-	
+
 			if self.KEY_TEST_WAIT in os.environ:
 				time.sleep( int( os.environ[self.KEY_TEST_WAIT] ) )
-		
-	
+
+
 	def _rewrite_pserve_config(	self,
 								config,
 								root_dir=DATASERVER_DIR,
 								port=PORT,
-								sync_changes=None, 
+								sync_changes=None,
 								out_dir="/tmp"):
-		
+
 		if not os.path.exists(config):
 			raise OSError('No pserve file %s' % config)
-		
+
 		ini = ConfigParser.SafeConfigParser()
 		ini.read(config)
-		
+
 		config_port = get_int_option(ini, name='http_port', default=PORT)
 		config_sync_changes = get_bool_option(ini, name='sync_changes', default=True)
 		sync_changes = config_sync_changes if sync_changes is None else sync_changes
-			
+
 		rewrite = config_port != port or sync_changes != config_sync_changes
-		if config_port != port:			
+		if config_port != port:
 			ini.set('DEFAULT', 'http_port', str(port))
-		
+
 		if sync_changes != config_sync_changes:
 			ini.set('DEFAULT', 'sync_changes', str(sync_changes))
-			
+
 		if rewrite:
 			if not os.path.exists(out_dir):
 				os.makedirs(out_dir)
-				
+
 			config = tempfile.mktemp(prefix="pserve.", suffix=".ini", dir=out_dir)
 			with open(config, "wb") as fp:
 				ini.write(fp)
-	
+
 		if sync_changes:
 			os.environ['DATASERVER_SYNC_CHANGES'] = 'True'
-			
+
 		return config
-			
+
 	def _rewrite_supervisor_config(self, config, command_prefix):
-		
+
 		if not os.path.exists(config):
 			raise OSError('No supervisord file %s' % config)
-		
+
 		ini = ConfigParser.SafeConfigParser()
 		ini.read(config)
 		for section in ini.sections():
 			if section.startswith("program:"):
 				command = ini.get(section, 'command')
 				ini.set(section, 'command', command_prefix + command)
-				
+
 		with open(config, "wb") as fp:
 			ini.write(fp)
-				
+
 	def _write_supervisor_config(self, root_dir=DATASERVER_DIR, pserve_ini_file=SERVER_CONFIG):
-		
+
 		root_dir = os.path.expanduser(root_dir)
 		if os.path.exists(root_dir):
 			shutil.rmtree(root_dir, ignore_errors=False)
-			
+
 		if not os.path.exists(pserve_ini_file):
 			raise OSError('No ini file %s' % pserve_ini_file)
-		
+
 		command = os.path.join(os.path.dirname(sys.executable), 'nti_init_env')
 		args = [command, root_dir, pserve_ini_file]
 		if subprocess.call(args) != 0:
 			raise OSError('Error while creating configurations')
-	
+
 		command_prefix = os.path.dirname(sys.executable) + '/'
 		visord = os.path.join(os.path.expanduser(root_dir), 'etc', 'supervisord_dev.conf')
 		self._rewrite_supervisor_config(visord, command_prefix)
-	
+
 	def _writer_supervisor_config_coverage(self, root_dir=DATASERVER_DIR, pserve_ini_file=SERVER_CONFIG,
 										   rcfile=COVERAGE_CONFIG):
-		
+
 		if not os.path.exists(rcfile):
 			raise OSError('No coverage file %s' % rcfile)
-		
+
 		self._write_supervisor_config(root_dir, pserve_ini_file)
 		visord = os.path.join(os.path.expanduser(root_dir), 'etc', 'supervisord_dev.conf')
 		command_prefix = ' '.join(['coverage', 'run', '--rcfile=%s' % rcfile, ''])
 		command_prefix = os.path.join(os.path.dirname(sys.executable), command_prefix)
 		self._rewrite_supervisor_config(visord, command_prefix)
-	
+
 
 	def terminate_server(self, block_interval_seconds=1, max_wait_secs=30):
 		if self.process or self.is_running():
@@ -225,8 +228,8 @@ class DataserverProcess(object):
 		if self.process or self.is_running():
 			if self._terminate_process(block_interval_seconds, max_wait_secs):
 				args = ['coverage', 'combine', "--rcfile=%s" % rcfile]
-				result = subprocess.call(args) == 0 
-			
+				result = subprocess.call(args) == 0
+
 			if result and report:
 				try:
 					dt = datetime.now()
@@ -248,7 +251,7 @@ class DataserverProcess(object):
 			result = self._wait_for_termination(block_interval_seconds, max_wait_secs)
 			self.process = None if result else self.process
 		return result
-		
+
 	def _wait_for_termination(self, block_interval_seconds=1, max_wait_secs=30):
 
 		elapsed = 0
@@ -261,5 +264,3 @@ class DataserverProcess(object):
 			return False
 
 		return True
-
-	
