53b2c17908ba3fb0ba0d89e7c3dd15c5a35e4521
Jason Madden <jason.madden@nextthought.com>
2012-07-15 18:19:50 +0000

    Ensure that newly created objects get a last modified time, not just when modified (TODO: This should be an event), but avoid doing this gratuitously if updating sharing doesn't change anything.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@8821 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/generalpurpose/testrunner/get_runner.py
+++ src/nti/integrationtests/generalpurpose/testrunner/get_runner.py
@@ -1,14 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+$Id: test_evolve15.py 8820 2012-07-15 17:34:42Z jason.madden $
+"""
+from __future__ import print_function, unicode_literals
+
 import urllib2
 
 from nti.integrationtests.generalpurpose.testrunner import BasicSeverOperation
+from hamcrest import assert_that, is_
 
 class GetObject(BasicSeverOperation):
 
 	def makeRequest(self, kwargs):
-		__traceback_info__ = self._traceback_info_
 		self.setValues(kwargs)
 
 		test_args = self.obj_setUp()
+		__traceback_info__ = self._traceback_info_, kwargs, test_args
 		assert test_args, "Invalid operation set up arguments"
 
 		self.set_time()
@@ -29,15 +37,15 @@ class GetObject(BasicSeverOperation):
 			self.obj_tearDown()
 
 			self.set_modification_time(parsed_body)
+			__traceback_info__ = self._traceback_info_, kwargs, test_args, request, ifModifiedSinceYes, ifModifiedSinceNo, parsed_body
+			assert_that( request.code, is_( self.responseCode['get'] ),
+					'this method was expecting a %d response, instead received %d' % (self.responseCode['get'], request.code) )
 
-			assert 	request.code == self.responseCode['get'], \
-					'this method was expecting a %d response, instead received %d' % (self.responseCode['get'], request.code)
+			assert_that( ifModifiedSinceYes, is_( self.responseCode['if_modified_since_yes'] ),
+					'this method was expecting a %d response, instead received %d' % (self.responseCode['if_modified_since_yes'], ifModifiedSinceYes) )
 
-			assert	ifModifiedSinceYes == self.responseCode['if_modified_since_yes'], \
-					'this method was expecting a %d response, instead received %d' % (ifModifiedSinceYes, self.responseCode['if_modified_since_yes'])
-
-			assert	ifModifiedSinceNo == self.responseCode['if_modified_since_no'], \
-					'this method was expecting a %d response, instead received %d' % (ifModifiedSinceNo, self.responseCode['if_modified_since_no'])
+			assert_that( ifModifiedSinceNo, is_( self.responseCode['if_modified_since_no'] ),
+					'this method was expecting a %d response, instead received %d' % (self.responseCode['if_modified_since_no'], ifModifiedSinceNo) )
 
 			self.objTest.testBody(parsed_body, self.postObjData['MimeType'], self.objResponse['postExpectedResponse'])
 
@@ -49,7 +57,7 @@ class GetObject(BasicSeverOperation):
 			self.obj_tearDown()
 			self.set_collection_modification_time()
 
-			assert 	error.code==self.responseCode['get'], \
-					'this method was expecting a %d response, instead received %d' % (self.responseCode['get'], error.code)
+			assert_that( error.code, is_( self.responseCode['get'] ),
+					'this method was expecting a %d response, instead received %d' % (self.responseCode['get'], error.code) )
 
 			self.check_unchanged_last_modified_time(preRequestTime=self.preRequestTime, collectionTime=self.lastModifiedCollection)
