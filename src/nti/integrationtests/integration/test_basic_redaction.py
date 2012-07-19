59a051456ab67643d0e1ed91ad9169fdb297484a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-07-19 01:29:51 +0000

    test for redactions
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@9024 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/integrationtests/integration/test_basic_redaction.py
@@ -0,0 +1,58 @@
+from __future__ import print_function, unicode_literals
+
+import time
+import unittest
+
+from nti.integrationtests import DataServerTestCase
+from nti.integrationtests.chat import generate_message
+from nti.integrationtests.integration import container_of_length
+
+from hamcrest import is_
+from hamcrest import assert_that
+from hamcrest import greater_than_or_equal_to
+
+class TestBasicRedactions(DataServerTestCase):
+	
+	owner = ('test.user.1@nextthought.com', DataServerTestCase.default_user_password)
+	target = ('test.user.2@nextthought.com', DataServerTestCase.default_user_password)
+
+	def setUp(self):
+		super(TestBasicRedactions, self).setUp()
+		self.container = 'test.user.container.%s' % time.time()
+		self.ds.set_credentials(self.owner)
+
+	def test_create_redaction(self):
+		st = generate_message()
+		created_obj =  self.ds.create_redaction(selectedText=st, replacementContent='redaction',
+												redactionExplanation='explanation', container=self.container,
+												 adapt=True)
+
+		assert_that(created_obj['selectedText'], is_(st))
+		assert_that(created_obj['replacementContent'], is_('redaction'))
+		assert_that(created_obj['redactionExplanation'], is_('explanation'))
+		
+	def test_search_shared(self):
+		self.ds.set_credentials(self.owner)	
+		
+		redaction =  self.ds.create_redaction(selectedText='Zangetsu', replacementContent='Katen Kyokotsu',
+											  redactionExplanation='Sogyo no Kotowari', container=self.container,
+											  adapt=True)
+		self.ds.share_object(redaction, self.target[0], adapt=True)
+		time.sleep(2)
+		
+		self.ds.set_credentials(self.target)
+		result = self.ds.search_user_content("Zangetsu")
+		assert_that(result, container_of_length(greater_than_or_equal_to(1)))
+		
+		result = self.ds.search_user_content("Kotowari")
+		assert_that(result, container_of_length(greater_than_or_equal_to(1)))
+		
+		result = self.ds.search_user_content("Kyokotsu")
+		assert_that(result, container_of_length(greater_than_or_equal_to(1)))
+		
+		self.ds.set_credentials(self.target)
+		result = self.ds.search_user_content("Engetsu")
+		assert_that(result, container_of_length(0))
+		
+if __name__ == '__main__':
+	unittest.main()
