32e01c592b8ff322e724835df0e9ddd00e9b489b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-25 20:36:05 +0000

    test to for user gen. data stream
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@10716 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/performance/eval/ugd.py
+++ src/nti/integrationtests/performance/eval/ugd.py
@@ -5,37 +5,24 @@ import time
 from concurrent.futures import ProcessPoolExecutor
 
 from nti.integrationtests.chat import generate_message
-from nti.integrationtests.dataserver.client import DataserverClient
-
 from nti.integrationtests.performance import IGNORE_RESULT
-from nti.integrationtests.performance.eval import init_server
-from nti.integrationtests.performance.eval import stop_server
 from nti.integrationtests.performance.eval import new_client
 from nti.integrationtests.performance.eval import generate_ntiid
 from nti.integrationtests.performance.eval import generate_random_text
 
-from hamcrest import assert_that
-from hamcrest import has_length
-from hamcrest import greater_than_or_equal_to
+from hamcrest import (assert_that, has_length, greater_than_or_equal_to)
 
 import logging
 logger = logging.getLogger(__name__)
-	
-# -----------------------------------
 
 def script_setup(context):
-	init_server(context)
 	prepare_containers(context)
-	
-def script_teardown(context):
-	stop_server(context)
 
-# -----------------------------------
-
-def prepare_container(endpoint, user, notes):
+def prepare_container(context, idx=1, notes=50):
+	client = new_client(context)
+	user = 'test.user.%s@nextthought.com' % idx
 	credentials = (user, 'temp001')
 	container = generate_ntiid(provider=user, nttype=generate_random_text())
-	client = DataserverClient(endpoint, credentials=credentials)
 	for _ in xrange(notes):
 		message = generate_message(k=3)
 		client.create_note(message, container=container, credentials=credentials)
@@ -43,16 +30,14 @@ def prepare_container(endpoint, user, notes):
 
 def prepare_containers(context):
 	result = {}
-	user = 'test.user.1@nextthought.com'
-	endpoint = context.endpoint
-	containers_sizes = getattr(context, 'containers_sizes', (50,)) 
+	containers_sizes = getattr(context, 'containers_sizes', {50:1}) 
 	
 	t = time.time()
 	logger.info("Preparing containers")
 	with ProcessPoolExecutor() as executor:
 		futures = []
-		for n in containers_sizes:
-			futures.append(executor.submit(prepare_container, endpoint, user, n))
+		for n, idx in containers_sizes.items():
+			futures.append(executor.submit(prepare_container, context, idx, n))
 			
 		for future in futures:
 			n, container = future.result()
@@ -61,8 +46,6 @@ def prepare_containers(context):
 	elapsed = time.time() - t
 	logger.info("Containers has been filled. (%.3f)s", elapsed)			
 	context['containers'] = result
-	
-# -----------------------------------
 
 def get_ugd(container_size, **kwargs):
 	context = kwargs['__context__']
@@ -74,8 +57,14 @@ def get_ugd(container_size, **kwargs):
 	if not container: 
 		return IGNORE_RESULT
 	
+	containers_sizes = getattr(context, 'containers_sizes', {50:1}) 
+	idx = containers_sizes.get(container_size, 1)
+	user = 'test.user.%s@nextthought.com' % idx
+	credentials = (user, 'temp001')
+	
 	# create a ds client
 	client = new_client(context)
+	client.set_credentials(credentials)
 	
 	ugd = client.get_user_generated_data(container)
 	assert_that(ugd['Items'], has_length(greater_than_or_equal_to(container_size)))
