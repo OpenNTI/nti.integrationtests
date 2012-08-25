8119626c211ea39ec68b72446195a7da0cef2696
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-25 19:18:48 +0000

    exapnd content search performance test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@10703 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/performance/eval/content_searcher.py
+++ src/nti/integrationtests/performance/eval/content_searcher.py
@@ -2,22 +2,26 @@ from __future__ import print_function, unicode_literals
 
 import random
 
+from nti.integrationtests.chat import generate_message
 from nti.integrationtests.performance import IGNORE_RESULT
 from nti.integrationtests.performance.eval import new_client
 from nti.integrationtests.chat.simulation import MAX_TEST_USERS
-from nti.integrationtests.performance.eval import generate_random_text
 
 _max_users = MAX_TEST_USERS
 		
 def search(*args, **kwargs):
 	context = kwargs['__context__']
-	#runner = kwargs['__runner__']
 	ntiids = context.ntiids
 	
 	idx = random.randint(1, _max_users)
 	credentials = ("test.user.%s@nextthought.com" % idx, 'temp001')
 	client = new_client(context)
 	client.set_credentials(credentials)
-	text = generate_random_text(random.randint(5, 15))
+	
+	text = None
+	splits = generate_message(k=3).split() 
+	while not text and text not in ('and', 'or', 'in', 'of'):
+		text = random.choice(splits)
+		
 	client.unified_search(text, random.choice(ntiids))	
 	return IGNORE_RESULT
