d19d9f2d39b76a5e8ed44bba389a87aacc8c59ea
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-27 22:36:23 +0000

    add a test for a notification circled event
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@10912 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/integrationtests/integration/test_notifications.py
@@ -0,0 +1,56 @@
+from __future__ import unicode_literals
+
+import time
+import uuid
+import unittest
+import threading
+
+from nti.integrationtests import DataServerTestCase
+from nti.integrationtests.chat.objects import BasicUser
+
+from hamcrest import assert_that, is_
+
+class _User(BasicUser):
+	def __init__(self, change_type='Circled', *args, **kwargs):
+		super(_User, self).__init__(*args, **kwargs)
+		self.change_type = change_type
+		self.found = False
+	
+	def data_noticeIncomingChange(self, change):
+		self.found = change.get('ChangeType', None) == self.change_type
+	
+	def __call__(self, *args, **kwargs):
+		try:
+			self.ws_connect()
+			self.heart_beats = 0
+			while self.heart_beats <=5 and not self.found:
+				self.nextEvent()
+		except:
+			pass
+		finally:
+			self.ws_close()
+		
+class TestNotifications(DataServerTestCase):
+
+	owner = ('test.user.1@nextthought.com', DataServerTestCase.default_user_password)
+	target = ('test.user.2@nextthought.com', DataServerTestCase.default_user_password)
+	
+	def setUp(self):
+		super(TestNotifications, self).setUp()
+		self.ds.set_credentials(self.owner)
+
+	def test_circled_event(self):
+		usr = _User(username=self.target[0], pasword=self.target[1], port=self.port)
+		t = threading.Thread(target=usr)
+		t.start()
+		time.sleep(1)
+		
+		name = '%s-%s' % (str(uuid.uuid4()).split('-')[0], time.time())
+		self.ds.create_friends_list_with_name_and_friends(name, friends=[self.target[0]])
+		
+		t.join()
+		assert_that(usr.found, is_(True))
+
+
+if __name__ == '__main__':
+	unittest.main()
