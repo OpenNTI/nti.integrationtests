e6cd8202246bb724459bd7dbf676a1111d6dd76e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-27 03:49:33 +0000

    update test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@10847 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/integration/test_user_search.py
+++ src/nti/integrationtests/integration/test_user_search.py
@@ -1,4 +1,3 @@
-import time
 import uuid
 
 from nti.integrationtests import DataServerTestCase
@@ -15,30 +14,7 @@ class TestUserSearch(DataServerTestCase):
 		self.container = 'container-%s' % uuid.uuid1()
 		self.users = [('%s.%s@nextthought.com' % (self.prefix, r), self.default_user_password) for r in range(15,19)]
 
-	def _create_note_object(self, client, note, container):
-		return client.create_object(note, objType='Note', container=container)
-
-	def _create_users_notes(self, container):
-		objects =[]
-		for f in self.users:
-			username = f[0]
-			note = 'Fake note owned by %s' % username
-			self.ds.clear_credentials()
-			self.ds.set_credentials(f)
-			objects.append(self.ds.create_note(note, self.container, adapt=True))
-		return objects
-
-	def _delete_user_notes(self, objects):
-		it = iter(objects)
-		for f in self.users:
-			self.ds.clear_credentials()
-			self.ds.set_credentials(f)
-			self.ds.delete_object(it.next())
-
 	def test_search_users(self):
-
-		objects = self._create_users_notes(self.container)
-		time.sleep(3)
 
 		self.ds.set_credentials(self.users[0])
 
@@ -55,8 +31,6 @@ class TestUserSearch(DataServerTestCase):
 
 		result = self.ds.execute_user_search("this_user_dne")
 		assert_that(result, container_of_length(0))
-
-		self._delete_user_notes(objects)
 
 if __name__ == '__main__':
 	import unittest
