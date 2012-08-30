06847727e8b1ef75f7349c7d24fbcaa686c11785
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-30 21:07:22 +0000

    compress source file
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@11080 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/nltk/__init__.py
+++ src/nti/integrationtests/nltk/__init__.py
@@ -1,5 +1,8 @@
 import os
-import nltk
+import gzip
+
+from nltk import Text
+from nltk import word_tokenize
 from nltk.model import NgramModel
 from nltk.probability import LidstoneProbDist
 
@@ -7,13 +10,12 @@ BUFFER = 6
 
 class NLTKMessageGenerator:
 
-	def __init__(self, filename, n = 3):
-		with open(filename, "r") as f:
-			raw = f.read()
-			tokens = nltk.word_tokenize(raw)
-			text = nltk.Text(tokens)
-			estimator = lambda fdist, bins: LidstoneProbDist(fdist, 0.2)
-			self.trigram_model = NgramModel(n, text, estimator=estimator);
+	def __init__(self, fileobj, n=3):
+		raw = fileobj.read()
+		tokens = word_tokenize(raw)
+		text = Text(tokens)
+		estimator = lambda fdist, bins: LidstoneProbDist(fdist, 0.2)
+		self.trigram_model = NgramModel(n, text, estimator=estimator);
 		
 	def generate_message(self, length):
 		ntext = self.trigram_model.generate(length + BUFFER)
@@ -25,6 +27,7 @@ _default_message_generator = None
 def default_message_generator():
 	global _default_message_generator
 	if not _default_message_generator:
-		name = os.path.join(os.path.dirname(__file__), "DanielDeronda.txt");
-		_default_message_generator = NLTKMessageGenerator(name)
+		name = os.path.join(os.path.dirname(__file__), "DanielDeronda.txt.gz");
+		with gzip.open(name, "rb") as src:
+			_default_message_generator = NLTKMessageGenerator(src)
 	return _default_message_generator
