27ff3352c68d1d1e765aca1250c1fdc0bd789619
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-09-20 20:31:32 +0000

    add a test to upload an image
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@12184 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/integration/test_basic_canvas.py
+++ src/nti/integrationtests/integration/test_basic_canvas.py
@@ -1,8 +1,11 @@
+import os
 import time
 import unittest
 
 from nti.integrationtests import DataServerTestCase
 from nti.integrationtests.contenttypes import Canvas
+from nti.integrationtests.utils import image_to_data_url
+from nti.integrationtests.contenttypes import CanvasUrlShape
 from nti.integrationtests.contenttypes import CanvasPolygonShape
 from nti.integrationtests.contenttypes import CanvasAffineTransform
 
@@ -33,6 +36,16 @@ class TestBasicCanvas(DataServerTestCase):
 		assert_that(created_obj['shapeList'][0]['transform']['d'], is_(0))
 		assert_that(created_obj['shapeList'][0]['transform']['tx'], is_(.25))
 		assert_that(created_obj['shapeList'][0]['transform']['ty'], is_(.25))
+		
+	def test_canvas_and_image(self):
 
+		source_file = os.path.join(os.path.dirname(__file__), "_class_image.jpg")
+		url = image_to_data_url(source_file)
+		urlShape = CanvasUrlShape(url=url)
+		canvas = Canvas(shapeList=[urlShape], container=self.container)
+		created_obj = self.ds.create_object(canvas, adapt=True)
+		urlShape = created_obj.shapeList[0]
+		assert_that(urlShape.url, is_not(None))
+		
 if __name__ == '__main__':
 	unittest.main()
