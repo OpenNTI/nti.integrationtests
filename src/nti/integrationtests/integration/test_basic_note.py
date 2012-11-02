7bafced90ad5f64c8112d311321665055b31ee0f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-02 21:45:44 +0000

    add some other basic tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@13718 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/integration/test_basic_note.py
+++ src/nti/integrationtests/integration/test_basic_note.py
@@ -8,13 +8,13 @@ from nti.integrationtests.contenttypes import Canvas
 from nti.integrationtests.contenttypes import CanvasPolygonShape
 from nti.integrationtests.contenttypes import CanvasAffineTransform
 
-from hamcrest import (is_, is_not, assert_that)
+from hamcrest import (is_, is_not, assert_that, has_length)
 
 class TestBasicNotes(DataServerTestCase):
 	
 	owner = ('test.user.1@nextthought.com', DataServerTestCase.default_user_password)
 	target = ('test.user.2@nextthought.com', DataServerTestCase.default_user_password)
-	string = 'a note to post'
+	string = u'a note to post'
 
 	def setUp(self):
 		super(TestBasicNotes, self).setUp()
@@ -22,104 +22,67 @@ class TestBasicNotes(DataServerTestCase):
 		self.container = 'test.user.container.%s' % time.time()
 		self.ds.set_credentials(self.owner)
 
-	# tests the five different types of notes that can be made
-	
 	def test_create_note_with_string(self):
-		# create the object to share
-		created_obj =  self.ds.create_note(self.string, self.container, adapt=True)
-		
-		# asserts that the object created has the string
-		assert_that(created_obj['body'][0], is_('a note to post'))
+		created_obj =  self.ds.create_note(self.string, self.container)
+		assert_that(created_obj['body'][0], is_(self.string))
+		assert_that(created_obj['id'], is_not(None))
 	
 	def test_create_note_with_string_in_array(self):
-		
-		# create the object to share
-		created_obj =  self.ds.create_note([self.string], self.container, adapt=True)
-		
-		# asserts that the object created has the string
-		assert_that(created_obj['body'][0], is_('a note to post'))
+		created_obj =  self.ds.create_note([self.string], self.container)
+		assert_that(created_obj['body'][0], is_(self.string))
 		
 	def test_create_note_with_string_and_object_in_array(self):
-		
 		created_canvas = self.create_canvas()
-		
-		# create the objec to share
-		created_obj =  self.ds.create_note([self.string, created_canvas], self.container, adapt=True)
-		
-		# asserts that the object created has a canvas object and the string
-		assert_that(created_obj['body'][0], is_('a note to post'))
+		created_obj =  self.ds.create_note([self.string, created_canvas], self.container)
+		assert_that(created_obj['body'][0], is_(self.string))
 		assert_that(created_obj['body'][1], is_(Canvas))
 		
 	def test_create_note_with_object_in_array(self):
-		
 		created_canvas = self.create_canvas()
-		
-		# create the object to share
-		created_obj =  self.ds.create_note([created_canvas], self.container, adapt=True)
-		
-		# asserts that the object created has a canvas object
-		assert_that(created_obj['body'][0], is_(Canvas))
-
-	def test_create_note_with_object(self):
-		
-		created_canvas = self.create_canvas()
-		
-		# create the object to share
-		created_obj =  self.ds.create_note(created_canvas, self.container, adapt=True)
-		
-		# asserts that the object created has a canvas object
+		created_obj =  self.ds.create_note([created_canvas], self.container)
 		assert_that(created_obj['body'][0], is_(Canvas))
 
 	def create_canvas(self):
-		
-		# creates a canvas
 		transform = CanvasAffineTransform(a=0, b=0, c=0, d=0, tx=.25, ty=.25)
 		polygon = CanvasPolygonShape(sides=4, transform=transform, container=self.container)
 		canvas = Canvas(shapeList=[polygon], container=self.container)
-		return self.ds.create_object(canvas, adapt=True)
-
-	createCanvas = create_canvas
+		return self.ds.create_object(canvas)
 	
 	def test_create_empty_note(self):
 		try:
-			self.ds.create_note(u'', self.container, adapt=True)
+			self.ds.create_note(u'', self.container)
 			self.fail('an empty not was created')
 		except:
 			pass
 		
-	def test_body_key_is_object(self):
-		# create the object to share
-		created_obj =  self.ds.create_note('A note to post', self.container, adapt=True)
+	def test_create_empty_container(self):
+		try:
+			self.ds.create_note(self.string, u'')
+			self.fail('a note in an empty container was created')
+		except:
+			pass
 		
-		# asserts that the shared object contains none.
-		assert_that(created_obj['body'][0]), is_("A reply to note")
-		assert_that(created_obj['inReplyTo'], is_(None))
-		assert_that(created_obj['references'], is_(None))
-		assert_that(created_obj['id'], is_not(None))
+	def test_inreply_to(self):
+		note_1 = self.ds.create_note(self.string, self.container)
 		
-	def test_storing_object_in_body(self):
+		nid = note_1['id']
+		note_2 =  self.ds.create_note('reply to n1', self.container, inReplyTo=nid)
 		
-		# create the object to share
-		transform = CanvasAffineTransform(a=0, b=0, c=0, d=0, tx=.25, ty=.25)
-		polygon = CanvasPolygonShape(sides=4, transform=transform, container=self.container)
-		canvas = Canvas(shapeList=[polygon], container=self.container)
-		created_obj = self.ds.create_object(canvas, adapt=True)
+		assert_that(note_2['body'][0]), is_("reply to n1")
+		assert_that(note_2['inReplyTo'], is_(nid))
 		
-		created_note = self.ds.create_note([created_obj], self.container, adapt=True)
-		assert_that(created_note['body'][0]['container'], is_(created_obj['container']))
-		assert_that(created_obj['id'], is_not(None))
+	def test_references(self):
+		nids =[]
+		for x in range(1,5):
+			references = nids if x == 4 else None
+			created_note = self.ds.create_note(['note %s' % x], self.container, references=references)
+			nids.append(created_note['id'])
+		assert_that(created_note['references'], has_length(len(nids)-1))
 		
-	def test_storing_text_and_object_in_body(self):
-		
-		# create the object to share
-		transform = CanvasAffineTransform(a=0, b=0, c=0, d=0, tx=.25, ty=.25)
-		polygon = CanvasPolygonShape(sides=4, transform=transform, container=self.container)
-		canvas = Canvas(shapeList=[polygon], container=self.container)
-		created_obj = self.ds.create_object(canvas, adapt=True)
-		
-		created_note = self.ds.create_note(['check this out' , created_obj], self.container, adapt=True)
-		assert_that(created_note['body'][1]['container'], is_(created_obj['container']))
-		assert_that(created_obj['id'], is_not(None))
+	def test_sharedWith(self):		
+		created_note = self.ds.create_note(self.string, self.container, sharedWith=[self.target[0]])
+		assert_that(created_note['sharedWith'], is_([self.target[0]]))
+
 
 if __name__ == '__main__':
 	unittest.main()
