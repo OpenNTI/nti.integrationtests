70e0f7e639e88c1bcc9cca42c69bd904146dfcbe
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-19 00:37:01 +0000

    enable test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@14123 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/integration/test_dynamic_friends_lists.py
+++ src/nti/integrationtests/integration/test_dynamic_friends_lists.py
@@ -1,15 +1,7 @@
-import time
 import unittest
 
-from nti.integrationtests import DataServerTestCase
-from nti.integrationtests.utils import generate_ntiid
-from nti.integrationtests.utils import generate_random_text
-from nti.integrationtests.contenttypes.users import DynamicFriendsList
-
 from nti.integrationtests.integration import test_friends_lists
-from hamcrest import (is_not, assert_that)
 
-@unittest.SkipTest
 class TestDynamicFriendsLists(test_friends_lists.TestBasicFriendsLists):
 	isDynamic = True
 	
