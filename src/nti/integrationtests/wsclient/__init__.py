24e04af3639a778fae49ff5a9b4ae24625a27061
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-01-15 18:37:50 +0000

    add source url
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@15078 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/wsclient/__init__.py
+++ src/nti/integrationtests/wsclient/__init__.py
@@ -17,6 +17,7 @@ Copyright (C) 2010 Hiroki Ohtani(liris)
 	License along with this library; if not, write to the Free Software
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+	https://github.com/liris/websocket-client/blob/master/websocket.py
 """
 
 import re
