0d57cdee255d34743c45e307a6e258e55e190548
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-01-15 16:34:33 +0000

    correctly specify the Origin url
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@15074 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/integration/test_user_preflight_create.py
+++ src/nti/integrationtests/integration/test_user_preflight_create.py
@@ -1,22 +1,28 @@
 import time
+import uuid
 import unittest
 
 from nti.integrationtests import DataServerTestCase
 
-from hamcrest import (assert_that,  has_entry, has_key)
+from hamcrest import (assert_that, has_entry, has_key, has_length, greater_than)
 
 class TestUserPreflightCreate(DataServerTestCase):
 
-	headers = {'Origin':'mathcounts.nextthought.com'}
+	headers = {'Origin':'https://mathcounts.nextthought.com'}
 
 	def setUp(self):
 		super(TestUserPreflightCreate, self).setUp()
 		self.container = 'test.upc.container.%s' % time.time()
 
 	def test_preflight_create(self):
-		data = {"opt_in_email_communication":True}
+		code =  str(uuid.uuid4()).split('-')[0]
+		username = u'u' + code
+		
+		data = {"opt_in_email_communication":True, 'Username':username}
 		d = self.ds.preflight_create_user(data)
 		assert_that(d, has_key('AvatarURLChoices'))
+		assert_that(d['AvatarURLChoices'], has_length(greater_than(0)))
+		
 		assert_that(d, has_key('ProfileSchema'))
 		assert_that(d, has_key('href'))
 		
@@ -31,13 +37,14 @@ class TestUserPreflightCreate(DataServerTestCase):
 		assert_that(d['password'], has_entry('required', True))
 		assert_that(d['password'], has_entry('name', 'password'))
 		
-		for key in ('affiliation', 'email', 'alias'):
+		for key in ('email', 'alias'):
 			assert_that(d, has_key(key))
-			assert_that(d[key], has_entry('required', False))
 			assert_that(d[key], has_entry('name', key))
 			assert_that(d[key], has_entry('type', 'string'))
 	
-		for key in ('avatarURL', 'birthdate', 'invitation_codes', 'opt_in_email_communication'):
+		assert_that(d['email'], has_entry('required', True))
+					
+		for key in ('avatarURL', 'birthdate', 'invitation_codes', 'opt_in_email_communication', 'affiliation'):
 			assert_that(d, has_key(key))
 			assert_that(d[key], has_entry('required', False))
 			assert_that(d[key], has_entry('name', key))
@@ -48,63 +55,3 @@ class TestUserPreflightCreate(DataServerTestCase):
 
 if __name__ == '__main__':
 	unittest.main()
-#
-#http://localhost:8081/dataserver2/users/@@account.preflight.create
-#{"opt_in_email_communication":true,"realname":"Carlos Sanchez"}:
-#
-#{"opt_in_email_communication":true,"realname":"Carlos Sanchez","email":"maletas@ou.edu"}:
-#
-#{"opt_in_email_communication":true,"realname":"Carlos Sanchez","email":"maletas@ou.edu","password":"saulo213"}:
-#
-#{"opt_in_email_communication":false,"realname":"Carlos Sanchez","email":"maletas@ou.edu","password":"saulo213","Username":"maletas"}:
-#
-#{.'AvatarURLChoices': [],
-# 'ProfileSchema': {'Username': {'min_length': 5,
-#                                'name': 'username',
-#                                'readonly': False,
-#                                'required': True,
-#                                'type': 'string'},
-#                   'affiliation': {'min_length': 0,
-#                                   'name': 'affiliation',
-#                                   'readonly': False,
-#                                   'required': False,
-#                                   'type': 'string'},
-#                   'alias': {'min_length': 0,
-#                             'name': 'alias',
-#                             'readonly': False,
-#                             'required': False,
-#                             'type': 'string'},
-#                   'avatarURL': {'min_length': 0,
-#                                 'name': 'avatarURL',
-#                                 'readonly': False,
-#                                 'required': False},
-#                   'birthdate': {'min_length': None,
-#                                 'name': 'birthdate',
-#                                 'readonly': False,
-#                                 'required': False,
-#                                 'type': 'date'},
-#                   'email': {'min_length': 0,
-#                             'name': 'email',
-#                             'readonly': False,
-#                             'required': False,
-#                             'type': 'string'},
-#                   'invitation_codes': {'name': 'invitation_codes',
-#                                        'readonly': False,
-#                                        'required': False,
-#                                        'type': 'list'},
-#                   'opt_in_email_communication': {'min_length': None,
-#                                                  'name': 'opt_in_email_communication',
-#                                                  'readonly': False,
-#                                                  'required': False,
-#                                                  'type': 'bool'},
-#                   'password': {'min_length': None,
-#                                'name': 'password',
-#                                'readonly': False,
-#                                'required': True},
-#                   'realname': {'min_length': 0,
-#                                'name': 'realname',
-#                                'readonly': False,
-#                                'required': True,
-#                                'type': 'string'}},
-# 'Username': 'carloszxz',
-# 'href': '/dataserver2/users/@@account.preflight.create'}
