92030189f84971d9e761306b6f9d75f61837558c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-02-25 21:21:41 +0000

    try to delete post (not sure why it won't work)
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@16263 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/integration/test_blogging.py
+++ src/nti/integrationtests/integration/test_blogging.py
@@ -3,7 +3,7 @@ import unittest
 from nti.integrationtests import DataServerTestCase
 from nti.integrationtests.integration import contains
 
-from hamcrest import (assert_that, is_, not_none, greater_than_or_equal_to)
+from hamcrest import (assert_that, is_, not_none, greater_than_or_equal_to, less_than)
 
 class TestBasicStream(DataServerTestCase):
 
@@ -14,7 +14,7 @@ class TestBasicStream(DataServerTestCase):
 		super(TestBasicStream, self).setUp()
 		self.ds.set_credentials(self.owner)
 
-	def test_create_post(self):
+	def xtest_create_post(self):
 		post = self.ds.create_blog_post('Shikai vs Bankai', 'No Zanpakuto in existence has a Shikai and a Bankai that use unrelated abilities')
 		assert_that(post, not_none())
 		assert_that(post.description, is_('Shikai vs Bankai'))
@@ -25,7 +25,7 @@ class TestBasicStream(DataServerTestCase):
 		contents = self.ds.get_blog_contents()
 		assert_that(contents, contains(post))
 
-	def test_user_cannot_change_sharing_on_blog_entry(self):
+	def xtest_user_cannot_change_sharing_on_blog_entry(self):
 		post = self.ds.create_blog_post('Bankai Ability', 'A mere upgrade?')
 		assert_that(post, not_none())
 		
@@ -40,6 +40,22 @@ class TestBasicStream(DataServerTestCase):
 		post = self.ds.create_blog_post('Ichigo', 'A fake Shinigami?', sharedWith=[self.target[0]])
 		assert_that(post, not_none())
 		assert_that(post.sharedWith, is_([]))
-	
+		
+	def test_create_edit_delete(self):
+		blog = self.ds.get_blog()
+		tc = blog.topicCount
+		
+		post = self.ds.create_blog_post('Spain', 'A no ending nightmare')
+		assert_that(post, not_none())
+		
+		ps = post.story
+		ps.body=['Uncertain Future']
+		ps = self.ds.update_object(ps)
+		
+		self.ds.delete_object(ps)
+		
+		blog = self.ds.get_blog()
+		assert_that(blog.topicCount, less_than(tc))
+
 if __name__ == '__main__':
 	unittest.main()
