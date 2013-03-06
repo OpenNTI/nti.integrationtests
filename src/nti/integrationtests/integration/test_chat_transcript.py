2f03743125803847ab0ec00ece686ccddac1f89f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-03-06 23:29:02 +0000

    put a delay in between posting messages. It seems that too fast not all the messages make it to the transcript
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@16765 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/integration/test_chat_transcript.py
+++ src/nti/integrationtests/integration/test_chat_transcript.py
@@ -15,7 +15,7 @@ class TestChatTranscript(HostUserChatTest):
 
 	def test_chat_transcript(self):
 		entries = 50
-		one, two = self._run_chat(self.container, entries, self.user_one, self.user_two)
+		one, two = self._run_chat(self.container, entries, self.user_one, self.user_two, delay=0.5)
 		for u in (one, two):
 			self.assert_(u.exception == None, "User %s caught exception '%s'" % (u.username, u.traceback))
 
