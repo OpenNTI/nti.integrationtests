3825dd5369eeee790479ae7d3bf993a5442df5aa
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-03-13 16:53:10 +0000

    fix test under new rules for user profile
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@17078 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/integration/test_user_preflight_create.py
+++ src/nti/integrationtests/integration/test_user_preflight_create.py
@@ -4,8 +4,8 @@
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
-#disable: accessing protected members, too many methods
-#pylint: disable=W0212,R0904
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
 
 import time
 import uuid
@@ -27,41 +27,40 @@ class TestUserPreflightCreate(DataServerTestCase):
 		self.container = 'test.upc.container.%s' % time.time()
 
 	def test_preflight_create(self):
-		code =  str(uuid.uuid4()).split('-')[0]
+		code = str(uuid.uuid4()).split('-')[0]
 		username = u'u' + code
-		
+
 		data = {"opt_in_email_communication":True, 'Username':username}
 		d = self.ds.preflight_create_user(data)
 
 		assert_that(d, has_key('AvatarURLChoices'))
 		assert_that(d['AvatarURLChoices'], has_length(greater_than(0)))
-		
+
 		assert_that(d, has_key('ProfileSchema'))
-		assert_that(d, has_key('href'))
-		
+
 		d = d.get('ProfileSchema')
 		for key in ('Username', 'realname'):
 			assert_that(d, has_key(key))
 			assert_that(d[key], has_entry('required', True))
 			assert_that(d[key], has_entry('name', key.lower()))
 			assert_that(d[key], has_entry('type', 'string'))
-			
+
 		assert_that(d, has_key('password'))
 		assert_that(d['password'], has_entry('required', True))
 		assert_that(d['password'], has_entry('name', 'password'))
-		
+
 		for key in ('email', 'alias'):
 			assert_that(d, has_key(key))
 			assert_that(d[key], has_entry('name', key))
 			assert_that(d[key], has_entry('base_type', 'string'))
-	
+
 		assert_that(d['email'], has_entry('required', True))
-					
+
 		for key in ('avatarURL', 'birthdate', 'invitation_codes', 'opt_in_email_communication', 'affiliation'):
 			assert_that(d, has_key(key))
 			assert_that(d[key], has_entry('required', False))
 			assert_that(d[key], has_entry('name', key))
-			
+
 		assert_that(d['birthdate'], has_entry('type', 'date'))
 		assert_that(d['invitation_codes'], has_entry('type', 'list'))
 		assert_that(d['opt_in_email_communication'], has_entry('type', 'bool'))
@@ -72,7 +71,7 @@ class TestUserPreflightCreate(DataServerTestCase):
 			self.fail(msg)
 		except:
 			pass
-		
+
 	def test_preflight_validate(self):
 		self._catch_test({'Username':''}, 'Username cannot be empty')
 		self._catch_test({'Username':'xx'}, 'Username too short')
@@ -81,6 +80,6 @@ class TestUserPreflightCreate(DataServerTestCase):
 		self._catch_test({'email':'xx'}, 'Invalid email')
 		self._catch_test({'role':'xx'}, 'Invalid role')
 		self._catch_test({'birthdate':'xx'}, 'Invalid birthdate')
-		
+
 if __name__ == '__main__':
 	unittest.main()
