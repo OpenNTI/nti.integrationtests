2b4cb5fe6fc0489364d9622db2cf68a53967ff59
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-05-31 15:53:46 +0000

    remove temp folder
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.integrationtests@19736 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/performance/b4bchanges/eval/shards/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import print_function, unicode_literals
-
-from nti.integrationtests.performance.eval.shards._ntids import is_running
-from nti.integrationtests.performance.eval.shards._ntids import terminate_server
-from nti.integrationtests.performance.eval.shards._ntids import prepare as start_server
