037aa75c6dc8f8c3be807e71811ad3e17261299b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-01-29 16:32:20 +0000

    open a new pkg to avoid possible collisions
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.integrationtests/trunk@31725 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/bigbang/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+"""
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
