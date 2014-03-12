3d75b9f303deaa90e8373b7297a20177bcd5dc8e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-03-12 01:17:49 +0000

    one more test for moderated messages
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.integrationtests/trunk@34680 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/integrationtests/integration/test_chat_moderated.py
+++ src/nti/integrationtests/integration/test_chat_moderated.py
@@ -7,10 +7,12 @@ __docformat__ = "restructuredtext en"
 #disable: accessing protected members, too many methods
 #pylint: disable=W0212,R0904
 
+import uuid
 import random
 import unittest
 
 from nti.integrationtests.chat.objects import Host
+from nti.integrationtests.chat.objects import User
 
 from nti.integrationtests.integration.user_chat_objects import HostUserChatTest
 
@@ -23,8 +25,10 @@ class TestModeratedChat(HostUserChatTest):
 		super(TestModeratedChat, self).setUp()
 		self.chat_users = self.user_names[:3]
 
+	def _create_user(self, username, **kwargs):
+		return Chatter(username=username, port=self.port, **kwargs)
+	
 	def test_moderated_chat(self):
-
 		entries = random.randint(10, 15)
 		users = self._run_chat(self.container, entries, *self.chat_users)
 		for u in users:
@@ -32,6 +36,9 @@ class TestModeratedChat(HostUserChatTest):
 
 		moderator = users[0]
 		chatters = users[1:]
+
+		credentials = (self.user_names[0], self.default_user_password)
+		self.ds.process_hypatia(-1, credentials=credentials)
 
 		all_recv=set()
 		for c in chatters:
@@ -42,18 +49,36 @@ class TestModeratedChat(HostUserChatTest):
 		for m in all_recv:
 			self.assert_(m not in moderator.moderated_messages, "Moderated message %s was received by a user" % m)
 
+		if not moderator.unmoderated:
+			return
+		text = moderator.unmoderated[0].text
+		credentials = (self.user_names[1], self.default_user_password)
+		self.ds.set_credentials(credentials)
+		result = self.ds.search_user_content('"%s"' % text)
+		self.assert_(result[u'Hit Count'] == 0, 'found unmoderated message')
+
 	def _create_host(self, username, occupants, **kwargs):
 		return Moderator(username=username, occupants=occupants, port=self.port, **kwargs)
 
+class Chatter(User):
+	def generate_message(self, *arg, **args):
+		return unicode(str(uuid.uuid4()))
+
 class Moderator(Host):
 
+	def __init__(self, *args, **kwargs):
+		super(Moderator, self).__init__(*args, **kwargs)
+		self.unmoderated = []
+
 	def chat_recvMessageForModeration(self, **kwargs):
-		super(Moderator, self).chat_recvMessageForModeration(**kwargs)
+		m = super(Moderator, self).chat_recvMessageForModeration(**kwargs)
 		self.heart_beats = 0
 		mid = kwargs['ID']
 		if random.random() > 0.7:
 			self.approveMessages(mid)
 			self.moderated_messages.pop(mid , None)
+		else:
+			self.unmoderated.append(m)
 
 	def wait_4_room(self):
 		super(Moderator, self).wait_4_room()
